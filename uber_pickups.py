c4b9576a5d0f7eb8168be0e97291a5e1f8c7df57
brandonharris-oss <brandon.harris@sjsu.edu>
2026-04-28 15:47:55 -0700

    Refactor and update Uber pickups Streamlit app

--- uber_pickups.py
+++ uber_pickups.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Brandon App: Uber pickups in NYC")
+st.title("Brandon's App: Uber pickups in NYC")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
